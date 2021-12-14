6492010e4c9086f2133046885b25d2c51812779b
nenadstuparcode <33831311+nenadstuparcode@users.noreply.github.com>
2021-12-14 14:58:13 +0100

    puppeteer fix
    puppeteer fix

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -313,9 +313,7 @@ exports.ticketPrint = [
 				path: "karte/generisani_2.pdf"
 			};
 
-			const browser = await puppeteer.launch({
-				args: ["--no-sandbox", "--disable-setuid-sandbox"],
-			});
+			const browser = await puppeteer.launch({headless: false, executablePath: "/usr/bin/chromium-browser", args: ["--no-sandbox"]});
 			const page = await browser.newPage();
 			await page.setContent(finalHtml);
 			await page.setViewport({ width: 1366, height: 768});
@@ -374,9 +372,7 @@ exports.sendToMail = [
 				path: "karte/generisani_2.pdf"
 			};
 
-			const browser = await puppeteer.launch({
-				args: ["--no-sandbox", "--disable-setuid-sandbox"],
-			});
+			const browser = await puppeteer.launch({headless: false, executablePath: "/usr/bin/chromium-browser", args: ["--no-sandbox"]});
 			const page = await browser.newPage();
 			await page.setContent(finalHtml);
 			await page.setViewport({ width: 1366, height: 768});
